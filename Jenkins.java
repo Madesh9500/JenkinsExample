fb7a1bb0f08541a1d3a813491818f07238c1d1de
Madesh9500 <129417596+Madesh9500@users.noreply.github.com>
2023-09-20 19:58:10 +0530

    Create Jenkins.java

---
+++ Jenkins.java
@@ -0,0 +1,9 @@
+class Jenkins
+  {
+    public static void main(String [] args)
+    {
+      System.out.println("Github jenkins");
+      System.out.println("Maddy");
+      System.out.println("Amalu");
+    }
+  }
